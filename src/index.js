4870979e88fe5b3a455f699f1d6738083fa83385
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-27 18:52:46 -0400

    extract tags

--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ const file = fs.readFileSync(filename, 'utf8')
 const isTask = line => line.startsWith('- ')
 const hasTag = line => line.includes('@')
 const lines = file.split(os.EOL).filter(isTask).filter(hasTag)
-console.log(lines)
-console.log('%d lines', lines.length)
 
+const lineToTagged = line => {
+  const reg = /.* @(example|feature|internal|support|slides|presentation|learning|hiring|blog)$/
+  const matches = reg.exec(line)
+  if (!matches) {
+    throw new Error(`Invalid line "${line}"`)
+  }
+
+  return {
+    line: matches[0].trim(),
+    tag: matches[1].trim()
+  }
+}
+
+// console.log(lines)
+// console.log('%d lines', lines.length)
+
+const tags = lines.map(lineToTagged)
+// console.log(tags)
+// console.log(lines)
+// console.log('%d lines', lines.length)
+
+const grouped = R.groupBy(R.prop('tag'), tags)
+// console.log(grouped)
+const counted = {}
+Object.keys(grouped).forEach(tag => {
+  counted[tag] = grouped[tag].length
+})
+console.log(counted)
